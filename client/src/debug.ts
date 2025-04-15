9f7de51765589ee3f7687e81bc7e59e352ea5fe6
landonh2026 <147776242+landonh2026@users.noreply.github.com>
2025-04-15 08:39:07 -0600

    Update debug.ts

--- client/src/debug.ts
+++ client/src/debug.ts
@@ -1,9 +1,6 @@
-/**
- * A currently blank class that can be used for debugging purposes
- */
 class Debugger {
     constructor() {
-
+        
     }
 
     public onFrameUpdate(deltaTime: number) {
